a3339c8bfdc18ee239fe1e0ee9dcda922690b9a8
Gaetano Meli <gaetano.meli95@gmail.com>
2026-09-25 20:50:26 +0200

    chore: use apex arredochef.it as canonical domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const body = Inter({
   display: 'swap',
 });
 
-const siteUrl = 'https://www.arredochef.it';
+const siteUrl = 'https://arredochef.it';
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
